531e44e6fc2977d653840e8b983e27a953893e31
alejandroesalas <alejandroesalas@gmail.com>
2019-08-18 17:08:28 -0500

    agregadas funciones para promover, actualizar y eliminar un usuario. estas funciones se enlazaron en los botones correspondientes para su respectivo llamado.

--- SIGPQR-Front-End/src/app/admin/components/users/users.component.ts
+++ SIGPQR-Front-End/src/app/admin/components/users/users.component.ts
@@ -4,6 +4,8 @@ import {ActivatedRoute, Router} from "@angular/router";
 import {AuthService} from "../../../services/authService/auth.service";
 import {Profile} from "../../../models/Profile";
 import {UserService} from "../../../services/user/user.service";
+import {Faculty} from "../../../models/Faculty";
+import {User} from "../../../models/User";
 
 @Component({
   selector: 'app-users',
@@ -12,7 +14,7 @@ import {UserService} from "../../../services/user/user.service";
 })
 export class UsersComponent implements OnInit {
     public docentes:Array<any>;
-    public currentUSer;
+    public currentUSer:User;
     public modal_id:string;
     public admin_profile = Profile.admin;
     public teacher_profile = Profile.teacher;
@@ -23,17 +25,17 @@ export class UsersComponent implements OnInit {
               private router: Router,
              ) {
     this.modal_id = "newUserModal";
+    this.currentUSer = new User(0,'','','','',0,'','','',0,0);
   }
 
   ngOnInit() {
     this.loadUsers();
   }
-  openModal(id: string) {
-    //this.currentUSer = selectedUser;
+  openModal(selectedUser,id: string) {
+    this.currentUSer = selectedUser;
     console.log(id);
     this.modalService.open(id);
   }
-
   closeModal(id: string) {
     this.modalService.close(id);
   }
@@ -50,6 +52,12 @@ export class UsersComponent implements OnInit {
 
     }
 
+  }
+  disableUser(docente){
+    console.log(docente);
+  }
+  promoverDocente(){
+    console.log(this.currentUSer);
   }
    createNewUser(){
 
