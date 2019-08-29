4f9132d70e87354dbf8ba99b899c6cb022a289ac
alejandroesalas <alejandroesalas@gmail.com>
2019-08-29 18:25:50 -0500

    Agregado Meotodo para cargar archivos en el controlador requestController

--- SIGPQR-Back-End/app/Http/Controllers/Request/RequestController.php
+++ SIGPQR-Back-End/app/Http/Controllers/Request/RequestController.php
@@ -2,142 +2,121 @@
 
 namespace App\Http\Controllers\Request;
 
+use App\AttachmentRequest;
 use App\Http\Controllers\ApiController;
 use App\Request as AppRequest;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Facades\Storage;
+ use lluminate\Support\Facades\File;
 
 class RequestController extends ApiController
 {
-public function index(){
-    $student = auth()->user();
-    $_requests = \App\Request::where('student_id', $student->id)
-        ->with('program')
-        ->with('program.coordinator')
-        ->get();
-    return $this->showAll($_requests);
-}
-public function uploadFiles(Request $request){
-    $files = $request->allFiles();
-    dd($files);
-    /*$validate = Validator::make($request->all(), [
-        'file0' => 'required|image|mimes:jpg,jpeg,png',
-    ]);
-    if (!files || $validate->fails()) {
-        $data = array(
-            'code' => 400,
-            'status' => 'error',
-            'message' => 'error al subir la imagen',
-            'errors'=>$validate->errors()
-        );
-    } else {
-        files_name = time().files->getClientOriginalName();
-        \Storage::disk('users')->put(files_name, \File::get(files));
-        $data = array(
-            'code' => 200,
-            'status' => 'succes',
-            'image' => files_name
-        );
-    }*/
-   // return response()->json($data, $data['code']);
-    private $rules =array(
-        'title'=>'required|max:200',
-        'description'=>'max:500',
-        'status'=> 'required',
-        'request_type_id'=>'required|integer',
-        'program_id'=>'required|integer',
-        'student_id'=> 'required|integer',
+    private $rules = array(
+        'title' => 'required|max:200',
+        'description' => 'max:500',
+        'status' => 'required',
+        'request_type_id' => 'required|integer',
+        'program_id' => 'required|integer',
+        'student_id' => 'required|integer',
     );
-    private $updateRules =array(
-        'title'=>'required|max:200',
-        'description'=>'max:500',
+    private $updateRules = array(
+        'title' => 'required|max:200',
+        'description' => 'max:500',
     );
-
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param Request $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
+    public function index()
     {
-        $json = $request->input('json', null);
-        if (!Empty($json)){
-            $params_array = array_map('trim', json_decode($json, true));
-            if (!Empty($params_array)){
-                    $validate = $this->checkValidation($params_array,$this->rules);
-
-                    if ($validate->fails()){
-                            return $this->errorResponse("datos no validos",$validate->errors());
-                    }else{
-                        $response = AppRequest::create($params_array);
-                        return $this->showOne($response);
-                    }
-            }else{
-                return $this->errorResponse('Datos Vacios!',422);
-            }
-        }else{
-            return $this->errorResponse('La estrucutra del json no es valida',422);
-        }
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param Request $request
-     * @param  \App\Request $request
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request1, AppRequest $response)
-    {
-        $json = $request1->input('json', null);
-        if (!Empty($json)){
-            $params_array = array_map('trim', json_decode($json, true));
-            if (!Empty($params_array)){
-                $validate = $this->checkValidation($params_array,$this->updateRules);
-                if ($validate->fails()){
-                    return $this->errorResponse("datos no validos",$validate->errors());
-                }else{
-                    $response->title = $params_array['title'];
-                    $response->description = $params_array['description'];
-                    if($response->isDirty()){
-                        return $this->errorResponse('se debe especificar al menos un valor',422);
-                    }
-                    $response->save();
-                    return $this->showOne($response);
-                }
-            }else{
-                return $this->errorResponse('Datos Vacios!',422);
-            }
-        }else{
-            return $this->errorResponse('La estrucutra del json no es valida',422);
-        }
+        $student = auth()->user();
+        $_requests =  AppRequest::where('student_id', $student->id)
+            ->with('program')
+            ->with('program.coordinator')
+            ->get();
+        return $this->showAll($_requests);
     }
 
     public function uploadFiles(Request $request)
     {
+        $attachments = array();
+        $student = auth()->user();
         $files = $request->allFiles();
-        dd($files);
-        /*$validate = Validator::make($request->all(), [
-            'file0' => 'required|image|mimes:jpg,jpeg,png',
-        ]);
-        if (!files || $validate->fails()) {
-            $data = array(
-                'code' => 400,
-                'status' => 'error',
-                'message' => 'error al subir la imagen',
-                'errors'=>$validate->errors()
-            );
-        } else {
-            files_name = time().files->getClientOriginalName();
-            \Storage::disk('users')->put(files_name, \File::get(files));
-            $data = array(
-                'code' => 200,
-                'status' => 'succes',
-                'image' => files_name
-            );
-        }*/
-    // return response()->json($data, $data['code']);
+        if ($files){
+            foreach ($files as $file){
+                 $attachmentRequest = new AttachmentRequest();
 
+                $fileName = time().$file->getClientOriginalName();
+                $attachmentRequest->name = $fileName;
+                if (!Storage::disk('upload')->exists($student->id_num)){
+                    Storage::disk('upload')->makeDirectory($student->id_num);
+                }
+                Storage::disk('upload')->put($student->id_num.'/'.$fileName,\File::get($file));
+                $attachmentRequest->route = 'upload'.$student->id_num.'/'.$fileName;
+                $attachments[]=$attachmentRequest;
+            }
+            return $this->showAll($attachments);
+
+        }else{
+            return $this->showMessage('error al subi archivos',400);
+        }
     }
+
+        /**
+         * Store a newly created resource in storage.
+         *
+         * @param Request $request
+         * @return \Illuminate\Http\Response
+         */
+        public function store(Request $request)
+        {
+            $json = $request->input('json', null);
+            if (!Empty($json)) {
+                $params_array = array_map('trim', json_decode($json, true));
+                if (!Empty($params_array)) {
+                    $validate = $this->checkValidation($params_array, $this->rules);
+
+                    if ($validate->fails()) {
+                        return $this->errorResponse("datos no validos", $validate->errors());
+                    } else {
+                        $response = AppRequest::create($params_array);
+                        return $this->showOne($response);
+                    }
+                } else {
+                    return $this->errorResponse('Datos Vacios!', 422);
+                }
+            } else {
+                return $this->errorResponse('La estrucutra del json no es valida', 422);
+            }
+        }
+
+        /**
+         * Update the specified resource in storage.
+         *
+         * @param Request $request
+         * @param \App\Request $request
+         * @return \Illuminate\Http\Response
+         */
+        public function update(Request $request1, AppRequest $response)
+        {
+            $json = $request1->input('json', null);
+            if (!Empty($json)) {
+                $params_array = array_map('trim', json_decode($json, true));
+                if (!Empty($params_array)) {
+                    $validate = $this->checkValidation($params_array, $this->updateRules);
+                    if ($validate->fails()) {
+                        return $this->errorResponse("datos no validos", $validate->errors());
+                    } else {
+                        $response->title = $params_array['title'];
+                        $response->description = $params_array['description'];
+                        if ($response->isDirty()) {
+                            return $this->errorResponse('se debe especificar al menos un valor', 422);
+                        }
+                        $response->save();
+                        return $this->showOne($response);
+                    }
+                } else {
+                    return $this->errorResponse('Datos Vacios!', 422);
+                }
+            } else {
+                return $this->errorResponse('La estrucutra del json no es valida', 422);
+            }
+        }
+
 }
