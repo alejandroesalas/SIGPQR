5942cd3735c20cbda5bc5ff266f5e6c848160cf7
alejandroesalas <alejandroesalas@gmail.com>
2019-09-01 18:00:45 -0500

    update project

---
+++ SIGPQR-Front-End/src/app/student/components/requests-timeline/requests-timeline.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-requests-timeline',
+  templateUrl: './requests-timeline.component.html',
+  styleUrls: ['./requests-timeline.component.css']
+})
+export class RequestsTimelineComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
